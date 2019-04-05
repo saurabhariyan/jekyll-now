b0f815300e9b9f51ff5c142ab98fc237b626619e
Saurabh Ariyan <saurabhkumar380@gmail.com>
2019-04-05 18:49:53 +0530

    test without pathPrefix

--- config.js
+++ config.js
@@ -12,6 +12,6 @@ module.exports = {
     twitter: "https://twitter.com/ryanafitzgerald",
     linkedin: "https://ca.linkedin.com/in/ryanafitzgerald",
   },
-  pathPrefix: "/devblog",
+  pathPrefix: "",
   siteUrl: "https://saurabhariyan.github.io/devblog/",
 };
